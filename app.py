d7eb16f6e0b22073cee76db69b2a0bf936c399b8
Jonathan Toy <jonathantoy@Jonathans-MacBook-Pro.local>
2020-03-30 19:31:02 -0400

    added error page templates

--- app.py
+++ app.py
@@ -354,7 +354,14 @@ def homepage():
     else:
         return render_template('home-anon.html')
 
+@app.errorhandler(404)
+def not_found_error(error):
+	return render_template('errors/404.html'), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('errors/500.html'), 500
 ##############################################################################
 # Turn off all caching in Flask
 #   (useful for dev; in production, this kind of stuff is typically
